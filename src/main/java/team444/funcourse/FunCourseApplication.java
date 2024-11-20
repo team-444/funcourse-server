04c7087e8959e157ec51741860458298e2576c78
junggyun <onlyplsson@gmail.com>
2024-11-21 02:16:05 +0900

    init

---
+++ src/main/java/team444/funcourse/FunCourseApplication.java
@@ -0,0 +1,13 @@
+package team444.funcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team444/funcourse/FunCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package team444.funcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
