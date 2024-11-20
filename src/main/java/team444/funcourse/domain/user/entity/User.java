3eef3a4e3e2608671083a3eff3f2033af2f5c820
junggyun <onlyplsson@gmail.com>
2024-11-21 02:51:30 +0900

    feat: 회원 엔티티, 리포지토리 개발

---
+++ src/main/java/team444/funcourse/domain/user/entity/User.java
@@ -0,0 +1,35 @@
+package team444.funcourse.domain.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    @Builder
+    public User(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
